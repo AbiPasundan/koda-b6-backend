a9771ce52ccf4682bc592bc6b0796aa662e6a997
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-02 14:49:10 +0700

    feat: get data user from database

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
 type test struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
+	Results any
 }
+
+type UserBeta struct {
+	Id        int    `json:"id"`
+	Full_Name string `json:"full_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Address   string `json:"address"`
+	Phone     string `json:"phone"`
+	Pictures  string `json:"pictures"`
+}
+
+type user struct {
+	Full_Name string `json:"full_name"`
+	Email     string `json:"email"`
+}
+
+// select users.id, users.full_name, users.email, users.password, users.address, users.phone, users.pictures from users;
 
 func CORSMiddleware() gin.HandlerFunc {
 	godotenv.Load()
@@ -28,14 +48,56 @@ func CORSMiddleware() gin.HandlerFunc {
 }
 
 func main() {
-	r := gin.Default()
-	r.Use(CORSMiddleware())
+
 	godotenv.Load()
 
+	connConfig, err := pgx.ParseConfig("")
+
+	if err != nil {
+		fmt.Println("err euy")
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := pgx.Connect(context.Background(), connConfig.ConnString())
+
+	if err != nil {
+		fmt.Println("err euy")
+		fmt.Println(err)
+		return
+	}
+
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+
 	r.GET("/", func(ctx *gin.Context) {
+		// query 1 ` select users.id, users.full_name, users.email, users.password, users. address, users.phone, users.pictures from users; `
+		rows, err := conn.Query(context.Background(), `
+			SELECT full_name, email FROM users
+		`)
+
+		if err != nil {
+			fmt.Println("err take data")
+			fmt.Println(err)
+			return
+		}
+
+		users, err := pgx.CollectRows(rows, pgx.RowToStructByName[user])
+
+		if err != nil {
+			fmt.Println("err take data")
+			fmt.Println(err)
+			ctx.JSON(http.StatusBadRequest, test{
+				Success: true,
+				Message: "Data User",
+			})
+			return
+		}
+
 		ctx.JSON(http.StatusOK, test{
 			Success: true,
 			Message: "Data User",
+			Results: users,
 		})
 	})
 
