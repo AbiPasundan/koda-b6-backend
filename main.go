7cf0428c75c684af3f0dcce97cd95554fdec3555
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-02 13:31:35 +0700

    feat: create control origin headers

--- main.go
+++ main.go
@@ -9,40 +9,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// func CORSMiddleware() gin.HandlerFunc {
-// 	return func(ctx *gin.Context) {
-// 		// c.Writer.Header().Set("Access-Control-Allow-Origin", "/")
-// 		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-// 		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-// 		// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-// 		ctx.Header("Access-Controll-Allow-Origin", "localhost:8888")
-// 		ctx.Header("Access-Controll-Allow-Headers", "content-type")
-
-// 		if ctx.Request.Method == "OPTIONS" {
-// 			ctx.AbortWithStatus(204)
-// 			return
-// 		}
-// 		ctx.Next()
-// 	}
-// }
+type test struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
 
 func CORSMiddleware() gin.HandlerFunc {
 	godotenv.Load()
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "localhost:5173")
-		// ctx.Header("Access-Control-Allow-Origin", "content-type")
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Controll-Allow-Headers", "Content-Type")
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.Data(http.StatusOK, "", []byte(""))
 		} else {
 			ctx.Next()
 		}
 	}
-}
-
-type test struct {
-	Success bool   `json:"success"`
-	Message string `json:"message"`
 }
 
 func main() {
