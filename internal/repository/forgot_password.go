978e9ed7856ca579af0c3656f05cec0d71acfaa4
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-11 10:24:47 +0700

    feat: create method GetUserByEmail, DeleteDataByEmail, CreateForgogtPasswordRequest

---
+++ internal/repository/forgot_password.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"backend/internal/models"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type ForgotPasswordRepository struct {
+	db *pgx.Conn
+}
+
+func NewForgotPasswordRepository(db *pgx.Conn) *ForgotPasswordRepository {
+	return &ForgotPasswordRepository{db: db}
+}
+
+func (f *ForgotPasswordRepository) GetUserByEmail(token string) (models.ForgotPassword, error) {
+	query := `SELECT id, user_id, token, created_at FROM forgot_password WHERE token = $1`
+	rows, err := f.db.Query(context.Background(), query, token)
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+	return pgx.CollectOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
+}
+
+func (f *ForgotPasswordRepository) DeleteDataByEmail(db *pgx.Conn, code string) *ForgotPasswordRepository {
+	query := `DELETE FROM forgot_password WHERE token = $1`
+	_, err := f.db.Query(context.Background(), query, code)
+	if err != nil {
+		return nil
+	}
+	return &ForgotPasswordRepository{db: db}
+}
+
+func (f *ForgotPasswordRepository) CreateForgogtPasswordRequest(db *pgx.Conn, token string) *ForgotPasswordRepository {
+	query := `INSERT INTO forgot_password (token) VALUES ($1)`
+	_, err := f.db.Query(context.Background(), query, token)
+	if err != nil {
+		return nil
+	}
+	return &ForgotPasswordRepository{db: db}
+}
