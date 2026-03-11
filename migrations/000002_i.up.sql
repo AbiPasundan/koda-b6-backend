d558dc6650be06b0c504ccf8dda690200d4a0f04
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-11 09:45:11 +0700

    feat: create migration for forgot password

--- migrations/000002_i.up.sql
+++ migrations/000002_i.up.sql
@@ -1,1 +1,9 @@
 ALTER TABLE users ADD COLUMN test VARCHAR(255);
+
+CREATE TABLE IF NOT EXISTS fotgot_password (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    token TEXT,
+    created_at TIMESTAMP,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
--- migrations/000003_i.down.sql
+++ migrations/000003_i.down.sql
@@ -1,2 +1,4 @@
+DROP TABLE IF EXISTS fotgot_password;
+
 ALTER TABLE users
 DROP COLUMN test;
