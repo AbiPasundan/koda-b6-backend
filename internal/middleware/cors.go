d62293aaf3e2e6986baa73bcea692582d4b95044
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-17 14:34:46 +0700

    feat: update cors middleware for development purpouse

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,9 +1,7 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -22,12 +20,33 @@ import (
 // 	}
 // }
 
+// func CORSMiddleware() gin.HandlerFunc {
+// 	godotenv.Load()
+// 	allowedOrigin := os.Getenv("FRONTEND_URL")
+// 	if allowedOrigin == "" {
+// 		allowedOrigin = "http://localhost:5173"
+// 	}
+
+// 	return func(ctx *gin.Context) {
+// 		ctx.Header("Access-Control-Allow-Origin", allowedOrigin)
+// 		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+// 		ctx.Header("Access-Control-Allow-Headers", "content-type,authorization")
+// 		ctx.Header("Access-Control-Allow-Credentials", "true")
+
+// 		if ctx.Request.Method == "OPTIONS" {
+// 			ctx.AbortWithStatus(204)
+// 			return
+// 		}
+// 		ctx.Next()
+// 	}
+// }
+
 func CORSMiddleware() gin.HandlerFunc {
 	godotenv.Load()
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", fmt.Sprintf(os.Getenv("FRONTEND_URL"), "http://localhost:5173"))
+		ctx.Header("Access-Control-Allow-Origin", "*")
 		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
-		ctx.Header("Access-Controll-Allow-Headers", "content-type,authorization")
+		ctx.Header("Access-Control-Allow-Headers", "content-type,authorization")
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.Data(http.StatusOK, "", []byte(""))
 		} else {
