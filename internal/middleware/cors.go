a5c2d5bafb9213f454b93a7badee4ecb0adb3a5e
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-17 11:09:17 +0700

    feat: update cors middleware

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,16 +2,31 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
+// func CORSMiddleware() gin.HandlerFunc {
+// 	godotenv.Load()
+// 	return func(ctx *gin.Context) {
+// 		ctx.Header("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
+// 		ctx.Header("Access-Controll-Allow-Headers", "Content-Type")
+// 		if ctx.Request.Method == "OPTIONS" {
+// 			ctx.Data(http.StatusOK, "", []byte(""))
+// 		} else {
+// 			ctx.Next()
+// 		}
+// 	}
+// }
+
 func CORSMiddleware() gin.HandlerFunc {
 	godotenv.Load()
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "/")
-		ctx.Header("Access-Controll-Allow-Headers", "Content-Type")
+		ctx.Header("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		ctx.Header("Access-Controll-Allow-Headers", "content-type,authorization")
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.Data(http.StatusOK, "", []byte(""))
 		} else {
