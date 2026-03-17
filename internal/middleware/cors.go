5019ea7cc03d3b2b017ae0501f0182a2d6ac9f92
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-03-17 14:14:33 +0700

    feat: add localhost

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
@@ -24,7 +25,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	godotenv.Load()
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
+		ctx.Header("Access-Control-Allow-Origin", fmt.Sprintf(os.Getenv("FRONTEND_URL"), "http://localhost:5173"))
 		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 		ctx.Header("Access-Controll-Allow-Headers", "content-type,authorization")
 		if ctx.Request.Method == "OPTIONS" {
