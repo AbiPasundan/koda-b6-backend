b828a03352f2a919d275be1a9c17b9ff5bca35bc
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-01 21:41:38 +0700

    fix: fix middleware on routes

--- internal/routes/product_cart_routes.go
+++ internal/routes/product_cart_routes.go
@@ -2,11 +2,23 @@ package routes
 
 import (
 	"backend/internal/handler"
+	"backend/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ProductCartRoutes(r *gin.Engine, h *handler.ProductCartHandler) {
-	// in landing page
-	r.POST("/detailproduct/addcart", h.AddCart)
+	user := r.Group("/user")
+	user.Use(middleware.JWTMiddleware(), middleware.RoleMiddleware("user", "admin"))
+	user.POST("/detailproduct/addcart", h.AddCart)
 }
+
+// {
+//	"user_id": 5,
+//	"product_id": 7,
+//	"quantity": 3,
+//	"product_name": "euy",
+//	"base_price": 1000,
+//	"variant_name": "no variant",
+//	"size_name": "no size"
+// }
